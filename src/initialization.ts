8c3021d9b691c0eede14c11eb756ba555d2633f0
Willian Monteiro <contato@willianmonteiro.com.br>
2023-07-07 16:41:20 -0300

    feat: init db function

---
+++ src/initialization.ts
@@ -0,0 +1,18 @@
+import InxDB from './InxDB';
+import logger from './utils/logger';
+
+export function initializeDB(instance: InxDB): void {
+	const request: IDBOpenDBRequest = indexedDB.open(instance['dbName']);
+
+	request.onerror = (event: Event) => {
+		logger.error('Failed to open IndexedDB:', (event.target as IDBRequest).error);
+	};
+
+	request.onsuccess = () => {
+		instance['db'] = request.result;
+	};
+
+	request.onupgradeneeded = () => {
+		instance['db'] = request.result;
+	};
+}
