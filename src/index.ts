396f2edc8b3eccd59190d34d1763defd8939785c
Willian Monteiro <contato@willianmonteiro.com.br>
2023-07-07 16:40:39 -0300

    feat: main file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { initializeDB } from './initialization';
+import { createCollection, getCollection } from './collection';
+import InxDB from './inxdb';
+
+export default InxDB;
+export { initializeDB, getCollection, createCollection };
