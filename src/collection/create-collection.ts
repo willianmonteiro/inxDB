83b9c27227b0250bc3d3253d9f9436beb39bfae2
Willian Monteiro <contato@willianmonteiro.com.br>
2023-07-13 17:18:10 -0300

    chore: improve logs

--- src/collection/create-collection.ts
+++ src/collection/create-collection.ts
@@ -2,11 +2,11 @@ import InxDB from '../inxdb';
 import logger from '../utils/logger';
 
 export function createCollection(instance: InxDB, collectionName: string): void {
-	if (!instance?.['db']) return logger.error('Database is not initialized.');
-	const version = instance['db'].version + 1; 
-	instance['db'].close(); 
+	if (!instance?.['db']) return logger.error('[create-collection]: Database is not initialized.');
+	const version = instance['db'].version + 1;
+	instance['db'].close();
 
-	const request = indexedDB.open(instance['dbName'], version); 
+	const request = indexedDB.open(instance['dbName'], version);
 
 	request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
 		const upgradedDB = (event.target as IDBOpenDBRequest).result;
@@ -15,11 +15,15 @@ export function createCollection(instance: InxDB, collectionName: string): void 
 		}
 	};
 
+	request.onblocked = () => {
+		logger.error('[create-collection]: IndexedDB is blocked. Please close other instances or tabs using the database.');
+	};
+
 	request.onsuccess = (event: Event) => {
-		instance['db'] = (event.target as IDBOpenDBRequest).result; 
+		instance['db'] = (event.target as IDBOpenDBRequest).result;
 	};
 
 	request.onerror = (event: Event) => {
-		logger.error('Error opening database:', (event.target as IDBOpenDBRequest).error);
+		logger.error('[create-collection]: Error opening database:', (event.target as IDBOpenDBRequest).error);
 	};
 }
--- src/collection/get-collection.ts
+++ src/collection/get-collection.ts
@@ -3,15 +3,15 @@ import logger from '../utils/logger';
 import { createCollection } from './create-collection';
 
 export function getCollection(instance: InxDB, collectionName: string, mode: IDBTransactionMode = 'readwrite'): IDBObjectStore | null {
-	if (!instance['db']) { 
-		logger.error('Database is not initialized.');
+	if (!instance['db']) {
+		logger.error('[get-collection]: Database is not initialized.');
 		return null;
 	}
 
-	if (!instance['db'].objectStoreNames.contains(collectionName)) { 
+	if (!instance['db'].objectStoreNames.contains(collectionName)) {
 		createCollection(instance, collectionName);
 	}
 
-	const transaction: IDBTransaction = instance['db'].transaction(collectionName, mode); 
+	const transaction: IDBTransaction = instance['db'].transaction(collectionName, mode);
 	return transaction.objectStore(collectionName);
 }
