22432b8ea1abd92f73aa160d2b761df466b9e0a1
Willian Monteiro <contato@willianmonteiro.com.br>
2023-07-27 22:27:32 -0300

    refactor: async transaction queue

--- src/queue.ts
+++ src/queue.ts
@@ -1,3 +1,5 @@
+import logger from './utils/logger';
+
 export default class TransactionQueue implements ITransactionQueue {
 	private queue: TransactionFunction[]; // Queue to hold transaction functions
 	private isProcessing: boolean; // Flag to track if the queue is currently being processed
@@ -7,30 +9,45 @@ export default class TransactionQueue implements ITransactionQueue {
 		this.isProcessing = false;
 	}
 
-	public enqueue(transactionFn: TransactionFunction): void {
-		this.queue.push(transactionFn);
-		this.processQueue();
+	public async enqueue(transactionFn: TransactionFunction): Promise<void> {
+		await new Promise<void>((resolve, reject) => {
+			const wrappedTransactionFn = async () => {
+				try {
+					await transactionFn();
+					resolve();
+				} catch (error) {
+					reject(error);
+				}
+				this.processQueue(); // Start processing the next transaction after the current one is done
+			};
+
+			this.queue.push(wrappedTransactionFn);
+
+			if (!this.isProcessing) {
+				this.processQueue();
+			}
+		});
 	}
 
-	private processQueue(): void {
-		if (this.isProcessing) {
-			return; // If queue is already being processed, exit
+	private async processQueue(): Promise<void> {
+		if (this.isProcessing || this.queue.length === 0) {
+			return;
 		}
-		const processNext = (): void => {
-			if (this.queue.length === 0) {
-				this.isProcessing = false;
-				return;
-			}
 
-			const transactionFn = this.queue.shift();
-			if (transactionFn) {
-				this.isProcessing = true;
-				transactionFn();
-				processNext();
-			}
-		};
+		this.isProcessing = true;
 
-		processNext();
+		try {
+			while (this.queue.length > 0) {
+				const transactionFn = this.queue.shift();
+				if (transactionFn) {
+					await transactionFn();
+				}
+			}
+		} catch (error) {
+			logger.error('[queue]: Error while executing operation:' + error);
+		} finally {
+			this.isProcessing = false;
+		}
 	}
 }
 
